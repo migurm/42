a5887a952fe15fb041e3f027a1237ec65c17de72
mrueda-m <mrueda-m@student.42malaga.com>
2024-08-28 13:21:44 +0200

    creación archivos iniciales

---
+++ get_next_line/get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mrueda-m <mrueda-m@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/28 11:18:46 by mrueda-m          #+#    #+#             */
+/*   Updated: 2024/08/28 11:25:06 by mrueda-m         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+//Funciones necesarias
+
+char    *get_next_line(int fd);
+
+#endif
